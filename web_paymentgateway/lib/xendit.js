4a1448aa7791cb58ef2b7291333afe9776687ac6
feltan01 <ftan010205@gmail.com>
2025-10-23 10:14:11 +0700

    Add change to repo

--- web_paymentgateway/lib/xendit.js
+++
@@ -1,4 +0,0 @@
-import Xendit from "xendit-node";
-
-const x = new Xendit({ secretKey: process.env.XENDIT_SECRET_KEY });
-export const { EWallet, Invoice } = x;
--- web_paymentgateway/pages/api/env-check.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    MONGODB_URI: process.env.MONGODB_URI || null,
-    XENDIT_SECRET_KEY: process.env.XENDIT_SECRET_KEY || null,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || null
-  });
-}
